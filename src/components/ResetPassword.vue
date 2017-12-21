177d59af386ec223532009c5039ab7486e7f1dd9
yamen <osaretinosayamen@outlook.com>
2017-12-21 10:16:45 +0100

    feature(reset-password): add reset password component
    [Start]

---
+++ src/components/ResetPassword.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <form>
+      <input type="email" name="email" placeholder="Please Enter Email" v-model="email">
+      <button type="submit" @click.prevent="handleClick()">
+        submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResetPassword',
+
+  data() {
+    return {
+      email: ''
+    };
+  },
+
+  methods: {
+    /**
+     * handles button click for reset password
+     * @returns {void}
+     */
+    handleClick() {
+      const { email  } = this;
+      console.log('attempts to reset password', email);
+    }
+  }
+}
+</script>
+
+<style></style>
